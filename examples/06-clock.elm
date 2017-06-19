89571cfed79c08dd1f8a6d232859c56dc2006129
perty <per@artcomputer.se>
2017-06-19 23:03:16 +0200

    Fixed the clock to show time correctly. Added minute and hour hands in other colours. Added text with date and time.

--- examples/06-clock.elm
+++ examples/06-clock.elm
@@ -1,4 +1,5 @@
-import Html exposing (Html)
+import Date
+import Html exposing (Html, div)
 import Svg exposing (..)
 import Svg.Attributes exposing (..)
 import Time exposing (Time, second)
@@ -57,16 +58,31 @@ subscriptions model =
 view : Model -> Html Msg
 view model =
   let
-    angle =
-      turns (Time.inMinutes model)
-
-    handX =
-      toString (50 + 40 * cos angle)
-
-    handY =
-      toString (50 + 40 * sin angle)
+    theDateNow = (Date.fromTime model)
+    angleSeconds = angleCalculator (Date.second theDateNow)  15.0 60
+    angleMinutes = angleCalculator (Date.minute theDateNow) 15.0 60
+    angleHours = angleCalculator ((Date.hour theDateNow) %  12) 3.0 12
   in
-    svg [ viewBox "0 0 100 100", width "300px" ]
-      [ circle [ cx "50", cy "50", r "45", fill "#0B79CE" ] []
-      , line [ x1 "50", y1 "50", x2 handX, y2 handY, stroke "#023963" ] []
+    div [] [
+        svg [ viewBox "0 0 100 100", width "300px" ]
+        [ circle [ cx "50", cy "50", r "45", fill "#0B79CE" ] []
+        , hand angleSeconds 40 "#023963"
+        , hand angleMinutes 40 "#1df14f"
+        , hand angleHours 30 "#f1361d"
+        ]
+        , div [] [
+            text (toString (Date.fromTime model))
+        ]
       ]
+
+angleCalculator: Int -> Float -> Float -> Float
+angleCalculator time offset ticksPerLap =
+    ((toFloat time) - offset) * 2.0 * pi / ticksPerLap
+
+hand: Float -> Float -> String -> Svg Msg
+hand angle length color =
+    let
+        xAngle = toString (50 + length * cos angle)
+        yAngle = toString (50 + length * sin angle)
+    in
+        line [ x1 "50", y1 "50", x2 xAngle, y2 yAngle, stroke color ] []
