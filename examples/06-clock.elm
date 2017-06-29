3388689b1ea2f62475dd9b7adf653ddf6d2e21c5
perty <per@artcomputer.se>
2017-06-29 22:45:26 +0200

    Added tick marks.

--- examples/06-clock.elm
+++ examples/06-clock.elm
@@ -59,25 +59,22 @@ view : Model -> Html Msg
 view model =
   let
     theDateNow = (Date.fromTime model)
-    angleSeconds = angleCalculator (Date.second theDateNow)  15.0 60
-    angleMinutes = angleCalculator (Date.minute theDateNow) 15.0 60
-    angleHours = angleCalculator (analogHour theDateNow) 15.0 60
+    angleSeconds = angleCalculator (Date.second theDateNow)
+    angleMinutes = angleCalculator (Date.minute theDateNow)
+    angleHours = angleCalculator (analogHour theDateNow)
+    clock = clockFn angleSeconds angleMinutes angleHours
   in
     div [] [
         svg [ viewBox "0 0 100 100", width "300px" ]
-        [ circle [ cx "50", cy "50", r "45", fill "#0B79CE" ] []
-        , hand angleSeconds 40 "#023963"
-        , hand angleMinutes 40 "#1df14f"
-        , hand angleHours 30 "#f1361d"
-        ]
+        clock
         , div [] [
             text (toString (Date.fromTime model))
         ]
       ]
 
-angleCalculator: Int -> Float -> Float -> Float
-angleCalculator time offset ticksPerLap =
-    ((toFloat time) - offset) * 2.0 * pi / ticksPerLap
+angleCalculator: Int -> Float
+angleCalculator time =
+    ((toFloat time) - 15.0) * 2.0 * pi / 60.0
 
 analogHour:  Date.Date -> Int
 analogHour theDateNow =
@@ -90,3 +87,31 @@ hand angle length color =
         yAngle = toString (50 + length * sin angle)
     in
         line [ x1 "50", y1 "50", x2 xAngle, y2 yAngle, stroke color ] []
+
+clockFn : Float -> Float -> Float -> List (Svg Msg)
+clockFn angleSeconds angleMinutes angleHours =
+    [
+       circle [ cx "50", cy "50", r "45", fill "#0B79CE" ] []
+    ]
+    ++ tickMarks
+    ++ [
+         hand angleSeconds 40 "#023963"
+        ,hand angleMinutes 40 "#1df14f"
+        ,hand angleHours 30 "#f1361d"
+        ]
+
+tickMarks: List (Svg Msg)
+tickMarks  =
+    List.map tick (List.range 1 60)
+
+tick : Int -> (Svg Msg)
+tick minute =
+    let
+        tickStart = if minute % 5 == 0 then 35 else 42
+        angle = angleCalculator minute
+        xEnd = toString (50 + 45 * cos angle)
+        yEnd = toString (50 + 45 * sin angle)
+        xStart = toString (50 + tickStart * cos angle)
+        yStart = toString (50 + tickStart * sin angle)
+    in
+        line [ x1 xStart, y1 yStart, x2 xEnd, y2 yEnd, stroke "#000000"] []
