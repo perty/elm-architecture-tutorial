9e16ce9441606925a13d1c55e4425f5874d74eff
perty <per@artcomputer.se>
2017-07-07 15:14:24 +0200

    Second clock example finished with SVG rotation.

--- examples/06-clock2.elm
+++ examples/06-clock2.elm
@@ -2,6 +2,7 @@ module Main exposing (..)
 
 import Date
 import Html exposing (Html, div)
+import Html.Attributes exposing (style)
 import Svg exposing (..)
 import Svg.Attributes exposing (..)
 import Time exposing (Time, second)
@@ -59,24 +60,109 @@ subscriptions model =
 
 view : Model -> Html Msg
 view model =
-    let
-        clock =
-            clockFn
-    in
-    div []
-        [ svg [ viewBox "0 0 100 100", width "300px" ]
-            clock
-        , div []
-            [ text (toString (Date.fromTime model))
+    div
+        [ Html.Attributes.style
+            [ ( "padding", "10px" )
+            , ( "border", "solid 1px" )
+            , ( "max-width", "100px" )
             ]
         ]
+        [ svg [ viewBox "0 0 100 100", width "100px" ]
+            (clockFn (Date.fromTime model))
+        ]
 
 
-clockFn : List (Svg Msg)
-clockFn =
-    [ g [ fill "green", stroke "black", transform "translate(50,50), rotate(0)" ]
-        [ circle [ cx "0", cy "0", r "50", fill "white" ] []
-        , polygon [ points "0,0 5,5 25,0 5, -5", transform "translate(15,0)" ] []
-        , line [ x1 "0", y1 "0", x2 "15", y2 "0" ] []
+clockFn : Date.Date -> List (Svg Msg)
+clockFn theDateNow =
+    [ g [ fill "black", stroke "black", transform "translate(50,50)" ]
+        (clockFace
+            ++ hourHand theDateNow
+            ++ minutesHand theDateNow
+            ++ secondsHand theDateNow
+        )
+    ]
+
+
+clockFace : List (Svg Msg)
+clockFace =
+    [ circle [ cx "0", cy "0", r "49", fill "white" ] []
+    ]
+        ++ tickMarks
+
+
+tickMarks : List (Svg Msg)
+tickMarks =
+    List.map tick (List.range 1 60)
+
+
+tick : Int -> Svg Msg
+tick minute =
+    let
+        tickStart =
+            if minute % 5 == 0 then
+                "35"
+            else
+                "42"
+
+        angle =
+            toString (minuteToAngle minute)
+    in
+    line [ x1 tickStart, y1 "0", x2 "49", y2 "0", transform ("rotate(" ++ angle ++ ")") ] []
+
+
+minuteToAngle : Int -> Float
+minuteToAngle minute =
+    (toFloat minute - 15.0) * 360 / 60.0
+
+
+hourHand : Date.Date -> List (Svg Msg)
+hourHand theDateNow =
+    let
+        angle =
+            minuteToAngle (hourMinute theDateNow)
+    in
+    [ g [ transform ("rotate(" ++ toString angle ++ ")") ]
+        [ polygon [ points "0,0 4,4 20,0 4, -4", transform "translate(10,0)" ] []
+        , xLine "0" "10"
         ]
     ]
+
+
+
+-- Which minute should the hour hand point to?
+
+
+hourMinute : Date.Date -> Int
+hourMinute theDateNow =
+    (Date.hour theDateNow % 12) * 5 + Date.minute theDateNow // 12
+
+
+minutesHand : Date.Date -> List (Svg Msg)
+minutesHand theDateNow =
+    let
+        angle =
+            minuteToAngle (Date.minute theDateNow)
+    in
+    [ g [ transform ("rotate(" ++ toString angle ++ ")") ]
+        [ polygon [ points "0,0 2,2 30,0 2, -2", transform "translate(5,0)" ] []
+        , xLine "0" "5"
+        ]
+    ]
+
+
+secondsHand : Date.Date -> List (Svg Msg)
+secondsHand theDateNow =
+    let
+        angle =
+            minuteToAngle (Date.second theDateNow)
+    in
+    [ g [ transform ("rotate(" ++ toString angle ++ ")") ]
+        [ xLine "-5" "40"
+        , circle [ cx "0", cy "0", r "5", fillOpacity "0", transform "translate(30,0)" ] []
+        ]
+    ]
+
+
+xLine : String -> String -> Svg Msg
+xLine start end =
+    line [ x1 start, y1 "0", x2 end, y2 "0" ] []
